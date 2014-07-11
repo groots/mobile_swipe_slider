2f2ceaa0834310b682d2595e746afc12e3210427
unknown <RootsG@HO-ROOTSG-7L.simon.com>
2014-07-11 16:54:17 -0400

    Added some js to log important information about the swiper elements. Added some ugly css to help me visually distinguish unique elements

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,20 @@
-function swiper(){
-	//fix this
-	console.log($);
-}
+$el = $(".swiper-gallery-wrapper"); 
 
-swiper();
+$el.on("swipeleft", function(){
+	rotateImage("left",".swiper-item");
+});
+
+$el.on("swiperight", function(){
+	rotateImage("right",".swiper-item");
+});
+
+function rotateImage(direction, dataGroup, sliderWrapper){
+	var dataSeries = $('dataGroup');
+	var $activeElement = $el.find(".active");
+	//dataSeries.each(function(){
+		//$el.animate({right: "300px"}, 500, 'ease-in-out');
+	//});
+	var $childEl = $activeElement.attr('class').split(" ")[0];
+	var avgElWidth = $el.width()/$el.children().size();
+	console.log(avgElWidth);
+}
